5af02e5d3a4c0216769c7442d9150733fa4d412c
Gen <archiherewego@gmail.com>
2020-05-04 23:51:10 +0900

    Update main and test codes
    こうすればそのままコピペで提出できる
    あとはテストコードをもっとシンプルに書けるようにすればオッケー

--- kotlin/test/abc165/Abc165bKtTest.kt
+++ kotlin/test/abc165/Abc165bKtTest.kt
@@ -1,3 +1,7 @@
+package abc165
+
+import TestData
+import abc165b
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
@@ -5,18 +9,15 @@ import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 
-class Abc165aKtTest {
+class Abc165bKtTest {
     companion object {
         @JvmStatic
         fun generateTestData() = listOf(
-            TestData("""7
-500 600""", """OK
+            TestData("103", """3
 """),
-            TestData("""4
-5 7""", """NG
+            TestData("1000000000000000000", """3760
 """),
-            TestData("""1
-11 11""", """OK
+            TestData("1333333333", """1706
 """)
         )
     }
@@ -28,8 +29,8 @@ class Abc165aKtTest {
         val stream = ByteArrayOutputStream()
         val testOut = PrintStream(stream)
         System.setOut(testOut)
-    
-        abc165a()
+        
+        abc165b()
         stream.toString() shouldBe data.expected
     }
 }
